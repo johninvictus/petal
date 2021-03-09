215a46db8c1e51078629695ac006a4feb55e9bfa
john.invictus <invictusnitude@gmail.com>
2021-03-09 13:17:22 +0300

    ci: added alpine support

---
+++ lib/petal_web/live/counter_live.ex
@@ -0,0 +1,15 @@
+defmodule PetalWeb.CounterLive do
+  use PetalWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, count: 0)}
+  end
+
+  def handle_event("increment", _params, socket) do
+    {:noreply, update(socket, :count, &(&1 + 1))}
+  end
+
+  def handle_event("decrement", _params, socket) do
+    {:noreply, update(socket, :count, &(&1 - 1))}
+  end
+end
