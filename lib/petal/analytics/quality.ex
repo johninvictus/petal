bf4f9d80ac06254aecf8dab32182e47f01c01600
john.invictus <invictusnitude@gmail.com>
2021-03-10 03:04:38 +0300

    ci: added changeset to capitalize words

--- lib/petal/analytics/quality.ex
+++ lib/petal/analytics/quality.ex
@@ -16,5 +16,13 @@ defmodule Petal.Analytics.Quality do
     |> validate_required([:name, :quality])
     |> validate_length(:name, min: 2, max: 100)
     |> validate_number(:quality, greater_than: 0, less_than_or_equal_to: 100)
+    |> capitalize_first_letter_of_name()
   end
+
+
+  defp capitalize_first_letter_of_name(%Ecto.Changeset{valid?: true, changes: %{name: name}} = changeset) do
+    put_change(changeset, :name, String.capitalize(name))
+  end
+
+  defp capitalize_first_letter_of_name(changeset), do: changeset
 end
